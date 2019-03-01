e4c0e18787a27c581df893497fcea8700f18f341
Allen M <tmracing008@yahoo.com>
2019-03-01 11:30:48 -0800

    move route into server

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require("express");
-
-const mongoose= require("mongoose");
+const path = require("path");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static("app/build"));
 }
 
 app.use(routes);
 
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/GoogleBookSearch"
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`App listening on port: ${PORT}`);
 });
