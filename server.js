42923293ea13dc43e0ace28c41169df7e17de265
Allen M <tmracing008@yahoo.com>
2019-03-01 11:45:23 -0800

    heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -7,9 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("app/build"));
