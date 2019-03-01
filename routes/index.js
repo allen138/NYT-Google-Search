fc54c7f463d77aab5325a350efce03ee93f7d76c
Allen M <tmracing008@yahoo.com>
2019-03-01 10:57:22 -0800

    change build path

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../app/build/index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 module.exports = router;
